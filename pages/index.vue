988e706a408643e6bb703691ff921bcd956d142e
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2022-02-22 14:46:52 +0200

    Cloudinary gallery added

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,12 @@
 <template>
 <div class="bg-white">
   <div class="pt-6">
-    <!-- Image gallery -->
-    <div class="mt-6 max-w-2xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
-      <div class="hidden aspect-w-3 aspect-h-4 rounded-lg overflow-hidden lg:block">
-        <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg" alt="Two each of gray, white, and black shirts laying flat." class="w-full h-full object-center object-cover">
-      </div>
-      <div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
-        <div class="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
-          <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg" alt="Model wearing plain black basic tee." class="w-full h-full object-center object-cover">
-        </div>
-        <div class="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
-          <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg" alt="Model wearing plain gray basic tee." class="w-full h-full object-center object-cover">
-        </div>
-      </div>
-      <div class="aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4">
-        <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg" alt="Model wearing plain white basic tee." class="w-full h-full object-center object-cover">
-      </div>
-    </div>
+
+    <!-- Cloudinary product gallery -->
+      <div 
+        id="my-gallery"
+
+      ></div>
 
     <!-- Product info -->
     <div class="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
@@ -96,6 +85,26 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  data(){
+    return {
+      gallery:null
+    }
+  },
+  mounted(){
+    this.gallery = cloudinary.galleryWidget({ 
+      container: "#my-gallery", 
+      cloudName: process.env.NUXT_ENV_CLOUDINARY_CLOUD_NAME, 
+      aspectRatio: "16:9",
+      mediaAssets: [
+        { tag: "npa-apple", },
+        { tag: "npa-apple", mediaType: "video" },
+      ] ,
+      videoProps: { playerType: "cloudinary" }  
+    });
+
+    this.gallery.render();
+
+    console.log("gallery",this.gallery);
+  }
 }
 </script>
